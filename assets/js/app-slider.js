51b4212308154967c4c5f8461e84e31f7ef96f82
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-06-30 00:48:58 +0300

    Create app-slider.js

---
+++ assets/js/app-slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.reviews').slick({
+        adaptiveHeight: true,
+        arrows: true,
+        speed:800,
+        easing: 'linear',
+        swipe: true,
+        touchMove: true,
+        waitForAnimate: false
+});
+});
+
